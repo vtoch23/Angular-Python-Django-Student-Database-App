6e529c1ceccdea1c152ac68f2b4478086c5dd501
Vanya Tochevska <Vanya@Mac.communityfibre.co.uk>
2024-11-25 18:53:19 +0000

    updated urls, added name

--- SchoolProject/urls.py
+++ SchoolProject/urls.py
@@ -5,8 +5,8 @@ from django.urls import re_path
 
 
 urlpatterns = [
-    re_path(r'^student$', views.studentApi),
-    re_path(r'^student$', views.studentApi),
-    re_path(r'^student/([0-9]+)$', views.studentApi),
+    re_path(r'^student$', views.studentApi, name='studentApi'),
+    re_path(r'^student$', views.studentApi, name='studentApi'),
+    re_path(r'^student/([0-9]+)$', views.studentApi, name='studentApi'),
     path('admin/', admin.site.urls),
 ]
